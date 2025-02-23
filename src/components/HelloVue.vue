4605c77a037b26e7de9ba80b9bbe2886544c5c71
rizkon86system <rizkon86system@gmail.com>
2025-02-23 17:48:22 +0700

    Belajar Vue 1

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,16 @@
+<script setup>
+console.info("Hello Vue");
+
+const hello = "<h1>Hello Vue</h1>";
+</script>
+
+<template>
+  <h1 class="hello">{{ hello }}</h1>
+  <div v-html="hello"></div>
+</template>
+
+<style scoped>
+.hello {
+  color: green;
+}
+</style>
