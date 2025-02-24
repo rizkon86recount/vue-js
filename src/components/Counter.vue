1b00c498b3b86969561b549767e45aebcf2c0baa
rizkon86system <rizkon86system@gmail.com>
2025-02-24 15:50:28 +0700

    update2

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+console.info("Load Component");
+let count = ref(0);
+
+function increment() {
+  console.info(`increment counter : ${count.value}`);
+  count.value++;
+}
+</script>
+
+<template>
+  <h1 id="naik">Counter : {{ count }}</h1>
+  <button v-on:click="increment">increment</button>
+</template>
+
+<style scoped></style>
--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -2,11 +2,23 @@
 console.info("Hello Vue");
 
 const hello = "<h1>Hello Vue</h1>";
+const data = {
+  class: "hello",
+  id: "hello",
+};
+const buttonDisabled = false;
+function helloFunction() {
+  return "Hello Rizkon Ganteng";
+}
 </script>
 
 <template>
-  <h1 class="hello">{{ hello }}</h1>
-  <div v-html="hello"></div>
+  <h1 v-bind="data">{{ hello.toUpperCase() }}</h1>
+  <h2>{{ 100 * 200 }}</h2>
+  <div :class="data.class" v-html="hello"></div>
+  <h2>{{ helloFunction() }}</h2>
+
+  <button :disabled="buttonDisabled">This is button</button>
 </template>
 
 <style scoped>
